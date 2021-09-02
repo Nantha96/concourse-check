bb91c6dab6a5e828c09f52f965d5856c36f46723
Nantha96 <nanthakmr2@gmail.com>
2021-09-02 16:52:17 +0530

    Update checkbranch.sh

--- scripts/checkbranch.sh
+++ scripts/checkbranch.sh
@@ -1,4 +1,5 @@
 cd $gitfolder
+git diff f1cd426a3a153d501b525ef6e2872073c9cdee8c 36803b3638509599178ee9b35b75038bb7908c71
 git show-branch --current | grep "develop"
 if [ $? -eq  1 ]; then 
     echo "The branch is Not Develop so Aborting....."
